2f7086246f5ce645566a72d137dbcad76179ebf9
AlvaroCS <alvaro.corral@innocv.com>
2019-09-15 18:23:00 +0200

    tunned component App

--- App.js
+++ App.js
@@ -9,7 +9,16 @@ class App extends Component {
     const user = {
       firstName: 'Zenchy',
       lastName: 'de Shinwo',
-      avatarUrl:''
+      avatarUrl:'https://cdn.pixabay.com/photo/2019/09/08/18/59/pumpkin-4461658_960_720.jpg'
+    }
+
+    const displayAvatar = (user) => {
+      if (user.avatarUrl){
+        return <img src={user.avatarUrl} />
+      } else {
+        return  <img src='https://cdn.pixabay.com/photo/2019/09/10/16/45/squirrel-4466635_960_720.jpg' width='300' height='300' />
+
+      }
     }
 
     const element = (<h2>Hello, {formatName(user)}!</h2>)
@@ -18,6 +27,7 @@ class App extends Component {
       <div className="App">
         <h1> Hello again reactive celluloid </h1>
         {element}
+        {displayAvatar(user)}
       </div>
     ) 
   }
