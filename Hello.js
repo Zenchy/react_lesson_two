86bbcb47ccde1a7ae9f1044dbc9a578ea04e3af0
AlvaroCS <alvaro.corral@innocv.com>
2019-09-15 17:45:44 +0200

    App component created

---
+++ Hello.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+import "./Hello.css"
+
+class Hello extends Component {
+  render(){
+    return(
+      <h2>POWERFULLOSO!!!</h2>
+    )
+  }
+}
+
+export default Hello
