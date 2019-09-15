9a11eed9928c00473d4747cad70d0872998bc655
AlvaroCS <alvaro.corral@innocv.com>
2019-09-15 17:59:58 +0200

    first embeded code

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React, {Component} from "react"
 import "./App.css"
+import Hello from "./Hello"
 
 class App extends Component {
   render(){
-    return(
+    const formatName = (user) => {return `${user.firstName} ${user.lastName}`}
+
+    const user = {
+      firstName: 'Zenchy',
+      lastName: 'de Shinwo',
+      avatarUrl:''
+    }
+
+    const element = (<h2>Hello, {formatName(user)}!</h2>)
+
+    return (
       <div className="App">
-        <h1>Hello Zenchy, you're fucking powerfull!!!</h1>
+        <h1> Hello again reactive celluloid </h1>
+        {element}
       </div>
-    )
+    ) 
   }
 }
 
